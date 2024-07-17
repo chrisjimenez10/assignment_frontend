aa8a361510fb73251e9b47bd666cc69903d0af73
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-07-17 14:44:27 -0500

    responsive design at 390px Home component

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
 
   return (
 
-    <main className="bg-orange-100 min-h-screen text-slate-950 text-wrap">
+    <main className="bg-white min-h-screen text-slate-950 text-wrap">
       <RecipesContext.Provider value={{fetchRecipesDatabase, recipeList}}>
       <Navbar />
       <Routes>
